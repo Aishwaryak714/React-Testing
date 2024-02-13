b33411aae1c46ee5f8e116bf5f6ca0642550b87a
Aishwarya <aishwarya-bhausaheb.kumtale@capgemini.com>
2024-02-13 12:36:41 +0530

    beforeAll, beforeEach, afterAll, afterEach

--- src/Form.spec.jsx
+++ src/Form.spec.jsx
@@ -1,16 +1,36 @@
 import { screen,render, fireEvent } from "@testing-library/react";
 import Form from "./Form";
 
-test("testing onChange event",()=>{
+beforeAll(()=>{
+        console.log("*********");
+})
+
+beforeEach(()=>{
+        console.log("+++++++++");
+})
+
+afterAll(()=>{
+        console.log("_____after_all______");
+})
+
+afterEach(()=>{
+        console.log("_____after_each______");
+})
+
+test("testing onChange event case 1",()=>{
+        console.log("1");
         render(<Form/>);
         let input = screen.getByPlaceholderText("Enter userName here");
          fireEvent.change(input,{target:{value:'a'}});
          expect(input.value).toBe("a");
 })
 
-test("testing Click Event",()=>{
+test("testing Click Event case 2",()=>{
+        console.log("2");
         render(<Form/>);
         const btn = screen.getByRole("button")
         fireEvent.click(btn);
         expect(screen.getByText("Updated Name")).toBeInTheDocument();
 })
+
+
